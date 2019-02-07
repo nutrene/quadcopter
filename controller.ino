f97548afd4f364cdc6fbe2259ecdf4257dbe7d07
vanilla <vanillariku@gmail.com>
2019-02-07 16:17:42 +0900

    調整器(重みの調整、重みの出力)

--- controller.ino
+++ controller.ino
@@ -4,15 +4,49 @@ int left_x = 0, left_y = 0;
 const int right_x_pin = A3, right_y_pin = A2;
 int right_x = 0, right_y = 0;
 
+////////// 調整器用 //////////
+                  // M1   M2   M4
+double weight[3] = {1.0, 1.0, 1.0};
+int pos = 0;
+bool ispush = false;
+/////////////////////////////
+
 void setup()
 {
     Serial.begin(9600);
+    
+    ////////// 調整器用 //////////
+    pinMode(5, INPUT);
+    pinMode(6, INPUT);
+    pinMode(7, INPUT);
+    ////////////////////////////
 }
-void loop()
-{  
+
+void loop(){
+    ////////// 調整器用 //////////
+    // -ボタン
+    if(digitalRead(5) == HIGH && !ispush){
+      weight[pos] -= 0.01;
+      ispush = true;
+    }
+    // +ボタン
+    if(digitalRead(7) == HIGH && !ispush){
+      weight[pos] += 0.01;
+      ispush = true;
+    }
+    // シフトボタン
+    if(digitalRead(6) == HIGH && !ispush){
+      pos = (pos + 1) % 3;
+      ispush = true;
+    }
+    // スイッチから離した時の処理
+    if(digitalRead(5) == LOW && digitalRead(6) == LOW && digitalRead(7) == LOW){
+      ispush = false;
+    }
+    //////////////////////////////
+
     left_x = analogRead(left_x_pin)-512;
     left_y = analogRead(left_y_pin)-512;
-
     right_x = analogRead(right_x_pin)-512;
     right_y = analogRead(right_y_pin)-512;
 
@@ -20,10 +54,18 @@ void loop()
     String string_left_y = (String)left_y + ',';
     String string_right_x = (String)right_x + ',';
     String string_right_y = (String)right_y;
+
     
     Serial.print(string_left_x);
     Serial.print(string_left_y);
     Serial.print(string_right_x);
     Serial.println(string_right_y);
+
+    ////////// 調整器用出力 //////////
+    Serial.print((pos == 0 ? "@" : "") + (String)weight[0] + ',');
+    Serial.print((pos == 1 ? "@" : "") + (String)weight[1] + ',');
+    Serial.println((pos == 2 ? "@" : "") + (String)weight[2]);
+    ///////////////////////////////
+    
     delay(50);
 }
--- quadcopter.ino
+++ quadcopter.ino
@@ -1,6 +1,6 @@
-#include<Servo.h>
+﻿#include<Servo.h>
 
-const int MIN = 1130, MAX = 2000;
+const int MIN = 1130, MAX = 2000, ATTENUATION_CONSTANT = 2, EMERGENCY_STOP_NUM = 60;
 int counter = 0;
 int left_x, left_y, right_x, right_y;
 char buff[30];
@@ -8,6 +8,8 @@ char buff[30];
 Servo motor1, motor2, motor3, motor4;
 
 int motor1_speed = 0, motor2_speed = 0, motor3_speed = 0, motor4_speed = 0;
+int emergency_allmotors_speed;
+int emergency_stop_counter = -999999999999;
 
 void show(String a, String b, String c, String d){
   Serial.print(a);
@@ -21,8 +23,8 @@ void show(String a, String b, String c, String d){
 
 void setup() {
   // PIN7=MOTOR1, PIN8=MOTOR2, PIN12=MOTOR3, PIN13=MOTOR4;
-  motor1.attach(7);
-  motor2.attach(8);
+  motor1.attach(10);
+  motor2.attach(11);
   motor3.attach(12);
   motor4.attach(13);
   //motor1.writeMicroseconds(1000);
@@ -43,20 +45,75 @@ void loop() {
     }else{
       counter++;
     }
+
+    // 左アナログスティックのY軸の入力を全てのモーターの回転速度に反映して垂直移動を行う。
+    motor1_speed = map(left_y, -472, 493, MIN, MAX);
+    motor2_speed = map(left_y, -472, 493, MIN, MAX);
+    motor3_speed = map(left_y, -472, 493, MIN, MAX);
+    motor4_speed = map(left_y, -472, 493, MIN, MAX);
+    ///////////////////////////////////////////////////
+
+    // 左アナログスティックのX軸の入力を全てのモーターに反映して回転運動を行う。
+    if(left_x <= -130){
+      left_x = max(-430, left_x); // left_xの最小値は-430に補正される。
+      double r = abs(left_x) / 1500.0 + 137 / 150.0;
+      motor1_speed *= r;
+      motor3_speed *= r;
+      motor2_speed *= 2 - r;
+      motor4_speed *= 2 - r;
+    }
+    if(130 <= left_x){
+      left_x = min(430, left_x); // left_xの最小値は+430に補正される。
+      double r = left_x / 1500.0 + 137 / 150.0;
+      motor2_speed *= r;
+      motor4_speed *= r;
+      motor1_speed *= 2 - r;
+      motor3_speed *= 2 - r;
+    }
+
+    ///// 緊急停止用カウント変数のカウントを0に戻す。 /////
+    emergency_stop_counter = 0;
+    ///////////////////////////////////////////////
+    
+  }else{
+
+    ///// 緊急停止用カウント変数にカウントする。 /////
+    emergency_stop_counter++;
+    Serial.print("Debug: ");
+    Serial.println(emergency_stop_counter);
+
+    ///// 通信失敗が定数EMERGENCY_STOP_NUM回続いた場合はモーターの回転を停止する。 /////
+    if(emergency_stop_counter >= EMERGENCY_STOP_NUM){
+      Serial.print("EMERGENCY STOPPING NOW.");
+      Serial.print(emergency_stop_counter);
+      Serial.print("\n");
+
+      /*
+      // emergency_allmotors_speedはホバリングが可能な回転数にするべきか。
+      if(emergency_stop_counter == EMERGENCY_STOP_NUM){
+        emergency_allmotors_speed = max(motor1_speed, max(motor2_speed, max(motor3_speed, motor4_speed)));
+        motor1_speed = emergency_allmotors_speed;
+        motor2_speed = emergency_allmotors_speed;
+        motor3_speed = emergency_allmotors_speed;
+        motor4_speed = emergency_allmotors_speed;
+      }
+      */
+
+      motor1_speed = 0;
+      motor2_speed = 0;
+      motor3_speed = 0;
+      motor4_speed = 0;
+    }
+    ///////////////////////////////////////////////////////////////////////
+    
   }
-  
-  // 0から1023の間で回転速度を決定する。
-  motor1_speed = map(left_y, -472, 493, MIN, MAX);
-  motor2_speed = map(left_y, -472, 493, MIN, MAX);
-  motor3_speed = map(left_y, -472, 493, MIN, MAX);
-  motor4_speed = map(left_y, -472, 493, MIN, MAX);
 
-  motor1_speed *= 0.993;
-  motor2_speed *= 0.993;
-
-  // 各ブラシレスモーターへ回転速度を伝達。
+  ///// 各ブラシレスモーターへ回転速度を送信する。 /////
   motor1.writeMicroseconds(motor1_speed);
   motor2.writeMicroseconds(motor2_speed);
   motor3.writeMicroseconds(motor3_speed);
   motor4.writeMicroseconds(motor4_speed);
+  //////////////////////////////////////////////
+
+  //delay(33);
 }
